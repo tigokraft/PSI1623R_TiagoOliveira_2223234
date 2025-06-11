00b20bd47598375936e941e6e8a24448af0dc006
tigokraft <tiagoco2008@gmail.com>
2025-06-11 21:40:47 +0100

    income DTO recurrence adition

--- src/api/api/DTOs/CreateIncome.cs
+++ src/api/api/DTOs/CreateIncome.cs
@@ -3,5 +3,6 @@ public class CreateIncomeDto
     public decimal Amount { get; set; }
     public DateTime Date { get; set; }
     public string Descr { get; set; }
-
+    public bool IsRecurring { get; set; } // new
+    public string? Recurrence { get; set; } // "daily", "weekly", "monthly"
 }
